62e2eaf232b7d5b4798f81790d49a1e496dc55a5
doulos76 <inojyes@gmail.com>
2019-05-11 12:57:42 +0900

    add model

---
+++ WeatherCastApp/WeatherCastApp/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  WeatherCastApp
+//
+//  Created by dave76 on 11/05/2019.
+//  Copyright © 2019 dave76. All rights reserved.
+//
+
+import Foundation
+
+let appKey = "98f55cc6-b0f9-4c23-9f2c-8d6dce36e3c6"
